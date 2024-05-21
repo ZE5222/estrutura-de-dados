a687e95ac46c5f78aae75c958a74a023633e3afe
ZE5222 <146848285+ZE5222@users.noreply.github.com>
2024-05-21 11:03:50 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,44 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "bubble_sort.h"
+#include "stack.h"
 
 int main(int argc, char* argv[]){
-    
-    FILE *fp = fopen(argv[1], "r");
 
-    if(fp == NULL){
-        printf("Erro ao abrir arquivo %s\n", argv[1]);
-        exit(1);
-    }
-    int i;
-    int qtd_n = 0;
-    int *vetor_n;
-    while( fscanf(fp, "%d\n", &i) != EOF){
-        qtd_n++;
-    }
-    vetor_n = malloc(sizeof(int)*qtd_n);
-    
-    rewind(fp);
-    i = 0;
-    while( fscanf(fp, "%d\n", &vetor_n[i]) != EOF){
-        i++;
-    }
+    StackNode* top = NULL;
 
-    fclose(fp);
+    push(&top, 'A');
+    push(&top, 'B');
+    push(&top, 'C');
 
-    bubble_sort(vetor_n, qtd_n);
+    display(top);
 
-    FILE* arq_saida = fopen("saida.txt", "w");
-    if(arq_saida == NULL){
-        printf("Erro ao abrir arquivo saida.txt");
-        exit(1);
-    }
+    printf("%c \n",top(&top));
 
-    for(i = 0; i < qtd_n; i++){
-        fprintf(arq_saida, "%d\n", vetor_n[i]);
-    }
+    display(top);
 
-    fclose(arq_saida);
+    printf("%c \n",peek(&top));
+
+    printf("%c ",top(&top));
+    printf("%c ",top(&top));
+   
 
     exit(0);
 }
